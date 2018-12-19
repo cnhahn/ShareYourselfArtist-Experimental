d1374dc01fc660ed9433bff38ffbb76b16b988a1
jaalsoto <jaalsoto@ucsc.edu>
2018-12-19 10:24:14 -0800

    Rebuilt the blogs page.

--- src/components/business/blogs.vue
+++ src/components/business/blogs.vue
@@ -2,6 +2,146 @@
 
   <v-container>
 
+    <h6 class="headline mb-4" style="font-weight: bold; color: black !important">Blogs</h6>
+
+    <!-- MOBILE SCREEN -->
+
+    <v-layout row wrap style="margin-bottom:-10px" v-if="screen_breakpoint">
+      <v-flex lg4 md4 sm5> <p class="body-2 ml-2"> Name </p> </v-flex>
+      <v-flex lg1 md1 hidden-sm-and-down> <p class="body-2"> Fans </p> </v-flex>
+      <v-flex lg6 md6 sm6> <p class="body-2"> Notes </p> </v-flex>
+    </v-layout>
+
+    <v-layout row wrap v-for="blog in user_info" v-bind:key="blog.url" v-if="!screen_breakpoint">
+
+      <v-flex xs12 mb-3>
+        <v-divider class="mt-1 mb-1"></v-divider>
+      </v-flex>
+
+      <v-flex xs1 mr-5>
+        <v-avatar>
+          <img :src="`${blog.url}`">
+        </v-avatar>
+      </v-flex>
+
+      <v-flex>
+
+        <h6 
+          v-if="userIsAuthanticated"
+          v-on:click="selectBusinessBlog(blog.business_name), $router.push('/business')"
+          class="headline" 
+          style="color:#FF7D27; margin-top: -5px; cursor: pointer;"
+          > 
+            {{blog.business_name}} 
+          </h6>
+
+        <h6 v-else class="headline" style="color:#FF7D27; margin-top: -5px"> {{blog.business_name}} </h6>
+
+        <p class="body-1" style="font-size:.8em !important">{{ getPassedTime(blog.upload_date) }}</p>
+        <p class="body-1 hidden-md-and-up" style="font-size:.8em !important; margin-top:-15px">Fans: {{ blog.follower_count }}</p>
+
+        <v-btn 
+          v-if="user_role === 'artist'"
+          @click='clicked_business({
+              	userId: blog.userId,
+              	business_email: blog.email,
+              	business_name: blog.business_name
+              })'
+          depressed
+          small
+          color = "primary"
+          style="margin-left: 0px; margin-top: -10px"
+        >
+          Select Blog
+        </v-btn>
+
+      </v-flex>
+
+      <v-flex xs12>
+            <p class="body-1" style="font-size: .85em !important; margin-top: 0px"><strong>About: </strong> <span>{{ blog.about }} </span> </p>
+            <p class="body-1" style="font-size: .85em !important; margin-top: -10px"><strong>The Good: </strong> <span>{{ blog.the_good }} </span> </p>
+            <p class="body-1" style="font-size: .85em !important; margin-top: -10px"><strong>Worth Knowing: </strong> <span>{{ blog.worth_knowing }} </span> </p>
+            <p class="body-1" style="font-size: .85em !important; margin-top: -10px"><strong>Additional Notes: </strong> <span>{{ blog.additional_notes }} </span> </p>
+          </v-flex>
+
+    </v-layout>
+
+    <!-- END OF MOBILE SCREEN -->
+
+    <!-- ALL OTHER SCREENS -->
+
+    <v-layout row wrap v-for="blog in user_info" v-bind:key="blog.url" v-if="screen_breakpoint">
+
+      <v-flex lg12 xs12 mb-3>
+        <v-divider class="mt-1 mb-1"></v-divider>
+      </v-flex>
+
+      <v-flex lg1 md1 sm1 mb-3>
+        <v-avatar>
+          <img :src="`${blog.url}`">
+        </v-avatar>
+      </v-flex>
+
+      <v-flex lg3 md3 sm4>
+
+        <h6 
+          v-if="userIsAuthanticated"
+          v-on:click="selectBusinessBlog(blog.business_name), $router.push('/business')"
+          class="headline" 
+          style="color:#FF7D27; margin-top: -5px; cursor: pointer;"
+          > 
+            {{blog.business_name}} 
+          </h6>
+
+        <h6 v-else class="headline" style="color:#FF7D27; margin-top: -5px"> {{blog.business_name}} </h6>
+
+        <p class="body-1" style="font-size:.8em !important">{{ getPassedTime(blog.upload_date) }}</p>
+        <p class="body-1 hidden-md-and-up" style="font-size:.8em !important; margin-top:-15px">Fans: {{ blog.follower_count }}</p>
+        
+        <v-btn 
+          v-if="user_role === 'artist'"
+          @click='clicked_business({
+              	userId: blog.userId,
+              	business_email: blog.email,
+              	business_name: blog.business_name
+              })'
+          depressed
+          small
+          color = "primary"
+          style="margin-left: 0px; margin-top: -10px"
+        >
+          Select Blog
+        </v-btn>
+
+
+      </v-flex>
+
+      <v-flex lg1 md1 hidden-sm-and-down>
+        <p class="body-2"> {{ blog.follower_count }} </p>
+      </v-flex>
+
+      <v-flex lg6 md6 sm6>
+
+        <v-layout row wrap>
+
+          <v-flex lg12>
+            <p class="body-1" style="font-size: .85em !important; margin-top: 0px"><strong>About: </strong> <span>{{ blog.about }} </span> </p>
+            <p class="body-1" style="font-size: .85em !important; margin-top: -10px"><strong>The Good: </strong> <span>{{ blog.the_good }} </span> </p>
+            <p class="body-1" style="font-size: .85em !important; margin-top: -10px"><strong>Worth Knowing: </strong> <span>{{ blog.worth_knowing }} </span> </p>
+            <p class="body-1" style="font-size: .85em !important; margin-top: -10px"><strong>Additional Notes: </strong> <span>{{ blog.additional_notes }} </span> </p>
+          </v-flex>
+
+        </v-layout>
+
+      </v-flex>
+
+    </v-layout>
+
+    <!-- END OF ALL OTHER SCREENS -->
+
+  </v-container>
+
+  <!--
   <h5 class="headline ml-3" style="font-weight: bold; color: black !important">Blogs</h5>
 
     <v-data-table
@@ -24,7 +164,7 @@
                 <a v-if="userIsAuthanticated" @click='selectBusinessBlog(props.item.business_name)' class="headline businessName" style="text-decoration:none;" >{{ props.item.business_name }}</a>
               </router-link>
               <p style="color:orange" v-if="!userIsAuthanticated" class="headline businessName">{{props.item.business_name}}</p>
-            <!-- <p>{{ getPassedTime(props.item.upload_date) }}</p> -->
+            COMMENT <p>{{ getPassedTime(props.item.upload_date) }}</p> END_COMMENT
             </div>
             <v-btn
               v-if="user_role === 'artist'"
@@ -69,8 +209,7 @@
       </td>
     </template>
     </v-data-table>
-
-  </v-container>
+    -->
 
 </template>
 
@@ -78,6 +217,7 @@
   export default {
     data() {
       return {
+        screen_breakpoint: false,
         dialog: false,
         businesses: [],
         headers: [
@@ -91,6 +231,16 @@
         ],
         business_name:'',
         select_business:'',
+      }
+    },
+    mounted() {
+      this.onResize()
+      window.addEventListener('resize', this.onResize, {passive: true})
+    },
+
+    beforeDestroy() {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', this.onResize, {passive: true})
       }
     },
     computed: {
@@ -112,6 +262,9 @@
       },
     },
     methods:{
+      onResize() {
+        this.screen_breakpoint = window.innerWidth > 700
+      },
       clicked_business(userId){
         const costOfBusiness = 1
         //this.$store.commit('set_art_being_submitted_is_selected', true)
@@ -180,28 +333,7 @@
     },
   }
 </script>
+
 <style scoped>
-  .divbottomline {
-    border-bottom: 1px solid #e0e0e0;
-  }
-  .table {
-    padding-top: 0px;
-    margin-bottom: 200px;
-  }
-  .businessName {
-    margin-top: 1vh;
-    margin-bottom: 0.25vh;
-  }
-  .businessUser {
-    margin-top: 1vh;
-  }
-  .businessNameHolder {
-    margin-left: 1vw;
-  }
-  .noteHolder {
-    margin-top: 1vh;
-  }
-  a{
-    text-decoration: none
-  }
+
 </style>
