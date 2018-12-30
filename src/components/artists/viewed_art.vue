deeea6c7d18da76860857b40e59aa23ad9caf3e6
scottdavis <okdogulu@ucsc.edu>
2018-12-30 13:57:40 -0800

    comment field added to the viewed_arts component

--- src/components/artists/viewed_art.vue
+++ src/components/artists/viewed_art.vue
@@ -1,47 +1,25 @@
 <template>
-  <v-container mt-3>
-    <v-layout row wrap>
-      <v-flex lg6 md6 sm12 xs12 ml-2 mr-2>
-        <img :src="this.art.url" alt="" width="100%">
-      </v-flex>
-      <v-flex lg4 md6 sm12 xs12 ml-2 mr-2>
-        <h2>{{this.art.art_title}}</h2>
-        <p>{{this.art.description}}</p>
-        <div class="buttons">
-          <v-btn depressed dark large color="black" @click="back">Back</v-btn>
-          <!-- <v-btn depressed large color="primary" style="width:120px" @click="submit_art(this.art)" router to="/blogs">Submit</v-btn> -->
-
-<!--           <div v-if="this.$store.getters.commenting_mode == false">
-            <v-btn flat  color="primary"
-            @click="comment_art()"
-            >Comment on this piece</v-btn>
-          </div>
-          <div v-if="this.$store.getters.commenting_mode == true">
-            <textarea style="border: 2px solid black" v-model="art.newComment" placeholder="Enter your comment here..."></textarea>
-            <v-btn flat  color="primary"
-            @click="save_comment(art.upload_date)"
-            >Post Comment</v-btn>
-            <v-btn flat  color="primary"
-            @click="comment_art()"
-            >Cancel</v-btn>
-          </div>
-
-          <div v-if="true" class = "commentSection">
-              <div v-for="comment in this.$store.getters.comments" class = "commentBox">
-                <h4><b>{{comment.author}}</b></h4>
-                <div class = "commentBody">
-                  <h3>{{comment.body}}</h3>
-                </div>
-              </div>
-            </div> -->
-
-        </div>
-      </v-flex>
-    </v-layout>
+  <v-container ml-3>
+  <v-layout class = "main-container" wrap>
+    <div class = "main-container">
+    <h2>{{this.art.art_title}}</h2>
+    <p>{{this.art.description}}</p>
+    </div>
+    <div class = "small-container">
+      <v-text-field
+          label="Comment"
+          single-line
+      ></v-text-field>
+    </div>
+      <div mb-5 class="small-container">
+      <v-btn  depressed dark large color="black" @click="back">Back</v-btn>
+    </div>
+    <img mt-5 :src="this.art.url" alt="" width="80%" height=100%>
+  </v-layout>
   </v-container>
-
 </template>
- <script>
+
+<script>
   export default {
         data() {
       return {
@@ -82,7 +60,7 @@
         // }
         // this.$store.commit('set_comments', newComment)
         // //console.log("artRef: ",artRef);
-        
+
         // return artRef.update({
         //   comments: this.$store.getters.comments
         // })
@@ -102,29 +80,20 @@
         this.$store.commit('set_art_being_submitted_is_selected',true)
         if(this.$store.state.business_being_submitted_is_selected == true){
            this.$router.push({
-              name: 'submit_result' 
+              name: 'submit_result'
             })
         }
       }
     }
   }
 </script>
-
-<style>
-  /* Don't know why I need this. */
-  .buttons{
-    margin-left: -8px;
-  }
-  .commentBody {
-    margin-left: 10px;
-  }
-  .commentSection {
-    /*background-color: lightgray;*/
-  }
-  .commentBox {
-    margin-top: 10px;
-    /*
-    border: 1px solid black;
-    */
-  }
+<style scoped>
+ .main-container {
+   width: 100%;
+ }
+ .small-container {
+   width: 80%;
+ }
 </style>
+
+
