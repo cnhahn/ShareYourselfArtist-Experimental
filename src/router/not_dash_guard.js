adf5a1acef952c35e42ff682bb8954fdf20868a9
Unknown <mdhsieh@ucsc.edu>
2019-02-08 22:48:44 -0800

    dash reroute attempt 3
    signout still has dash components, guard fails if already at homepage in new tab

--- src/router/not_dash_guard.js
+++ src/router/not_dash_guard.js
@@ -9,7 +9,21 @@ console.log('userId: ' + localStorage.getItem('userId'))
 const key = localStorage.getItem('userId')
  
     if (!(key>=0)) {
-        next()
+        if (from.name != 'Home' && from.name != 'sign_in' && from.name != null && to.name == 'sign_in')
+        {
+            console.log('need to refresh page')
+            // force refresh, or else signin page has dash components
+            // not great temp
+            //to.push('/sign_in')
+            //to.go(0)
+            //to.reload()
+            //next('sign_in')
+            next()
+        }
+        else
+        {
+            next()
+        }
     }
     else if (key >= 0 && (to.name == 'Home' || to.name == 'sign_in') )
     {
