3d9f96a2e45e441491083d19b14e42a03e902208
Unknown <mdhsieh@ucsc.edu>
2019-02-20 09:55:17 -0800

    display all comments
    now displays all comments on art piece, back button correct, clear input after sending, can send multiple comments

--- src/components/artists/viewed_art.vue
+++ src/components/artists/viewed_art.vue
@@ -5,6 +5,15 @@
     <h2>{{this.art.art_title}}</h2>
     <p>{{this.art.description}}</p>
     </div>
+
+    <p v-for="comment_field in new_comments_field"
+    :key="comment_field.comment">
+    <pre>
+    {{comment_field.from}} says: {{comment_field.comment}}
+    </pre> 
+    </p>
+    <v-spacer></v-spacer>
+
     <div class = "small-container">
       <v-text-field
           label="Comment"
@@ -12,14 +21,6 @@
           v-model="comment"
       ></v-text-field>
     </div>
-
-    <p v-for="comment_field in new_comments_field"
-    :key="comment_field.from">
-    <pre>
-    {{comment_field.from}} says: {{comment_field.comment}}
-    </pre> 
-    </p>
-    <v-spacer></v-spacer>
 
       <div mb-5 class="small-container">
       <v-btn v-if= "!this.comment.length"   disabled  large>Send</v-btn>
@@ -121,35 +122,45 @@ import * as firebase from 'firebase'
 
       },
       back(){
-        window.history.back();
+        //window.history.back();
+        this.$router.push({
+        path:'/artist_dashboard'
+        })
       },
        save_comment(art){
         const artists_arts =  this.$store.getters.viewed_arts
         const commented_art = artists_arts.filter(function (the_art) {
           return the_art.upload_date == art.upload_date
         } )
-        //let new_comments_field = []
         let commentor =  this.$store.getters.signed_in_user.email
         if (commented_art[0].comments ===  undefined){
           this.new_comments_field.push({from: commentor, comment:this.comment})
-          //console.log('1st comment')
         } else {
-          this.new_comments_field = [...commented_art[0].comments,{from: commentor, comment:this.comment} ]
-          //console.log('every other comment')
+          // this.new_comments_field = [...commented_art[0].comments,{from: commentor, comment:this.comment} ]
+          this.new_comments_field.push({from: commentor, comment:this.comment})
         }
         let upload_date = parseInt(this.art.upload_date)
         this.$store.dispatch('update_art_comments', {upload_date:upload_date,comments: this.new_comments_field}).then(this.snackbar = true)
+
+        this.clear_comment()
+      },
+
+      clear_comment()
+      {
+        this.comment =''
       },
 
       load_comments(art)
       {
+        let comments_field = this.new_comments_field
+        
         const artists_arts =  this.$store.getters.viewed_arts
         const commented_art = artists_arts.filter(function (the_art) {
           return the_art.upload_date == art.upload_date
         } )
         
         let upload_date = parseInt(this.art.upload_date)
-        //console.log('loading comments')
+      
         let db = firebase.firestore()
         let id_of_art = db.collection('art').where('upload_date', '==', upload_date)
 
@@ -159,14 +170,13 @@ import * as firebase from 'firebase'
         } else {
           // go through all results
           results.forEach(function (doc) {
-            //let docId = doc.id
-            //console.log('loading action doc id ', docId)
+            
             let data = doc.data()
             let comments = data.comments
             for (var i = 0; i < comments.length; i++)
             {
-              console.log('from: ', comments[i].from, 'comm: ', comments[i].comment)
-              this.new_comments_field.push(comments[i])
+              //console.log('from: ', comments[i].from, 'comm: ', comments[i].comment)
+              comments_field.push(comments[i])
             }
 
           })
