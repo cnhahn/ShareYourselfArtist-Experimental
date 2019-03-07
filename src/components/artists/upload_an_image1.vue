e40abb1fcc168dc9481656bd92fae80d367e4dbd
Unknown <mdhsieh@ucsc.edu>
2019-03-07 13:50:27 -0800

    resize image gooogd
    able to resize image using canvas, max dimensions 1200x630. If the image is smaller, there's no resize.
    
    GIFS need to be checked as well

--- src/components/artists/upload_an_image1.vue
+++ src/components/artists/upload_an_image1.vue
@@ -145,189 +145,6 @@
 </template>
 
 <script>
-/*** Start ImageTools.js, https://gist.github.com/dcollien/312bce1270a5f511bf4a#file-imagetools-es6 */
-'use strict';
-
-if (typeof exports === "undefined") {
-    var exports = {};
-}
-
-if (typeof module === "undefined") {
-   var module = {};
-}
-
-Object.defineProperty(exports, '__esModule', {
-    value: true
-});
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-var hasBlobConstructor = typeof Blob !== 'undefined' && (function () {
-    try {
-        return Boolean(new Blob());
-    } catch (e) {
-        return false;
-    }
-})();
-
-var hasArrayBufferViewSupport = hasBlobConstructor && typeof Uint8Array !== 'undefined' && (function () {
-    try {
-        return new Blob([new Uint8Array(100)]).size === 100;
-    } catch (e) {
-        return false;
-    }
-})();
-
-var hasToBlobSupport = typeof HTMLCanvasElement !== "undefined" ? HTMLCanvasElement.prototype.toBlob : false;
-
-var hasBlobSupport = hasToBlobSupport || typeof Uint8Array !== 'undefined' && typeof ArrayBuffer !== 'undefined' && typeof atob !== 'undefined';
-
-var hasReaderSupport = typeof FileReader !== 'undefined' || typeof URL !== 'undefined';
-
-var ImageTools = (function () {
-    function ImageTools() {
-        _classCallCheck(this, ImageTools);
-    }
-
-    _createClass(ImageTools, null, [{
-        key: 'resize',
-        value: function resize(file, maxDimensions, callback) {
-            if (typeof maxDimensions === 'function') {
-                callback = maxDimensions;
-                maxDimensions = {
-                    width: 640,
-                    height: 480
-                };
-            }
-
-            var maxWidth = maxDimensions.width;
-            var maxHeight = maxDimensions.height;
-
-            if (!ImageTools.isSupported() || !file.type.match(/image.*/)) {
-                callback(file, false);
-                return false;
-            }
-
-            if (file.type.match(/image\/gif/)) {
-                // Not attempting, could be an animated gif
-                callback(file, false);
-                // TODO: use https://github.com/antimatter15/whammy to convert gif to webm
-                return false;
-            }
-
-            var image = document.createElement('img');
-
-            image.onload = function (imgEvt) {
-                var width = image.width;
-                var height = image.height;
-                var isTooLarge = false;
-
-                if (width > height && width > maxDimensions.width) {
-                    // width is the largest dimension, and it's too big.
-                    height *= maxDimensions.width / width;
-                    width = maxDimensions.width;
-                    isTooLarge = true;
-                } else if (height > maxDimensions.height) {
-                    // either width wasn't over-size or height is the largest dimension
-                    // and the height is over-size
-                    width *= maxDimensions.height / height;
-                    height = maxDimensions.height;
-                    isTooLarge = true;
-                }
-
-                if (!isTooLarge) {
-                    // early exit; no need to resize
-                    callback(file, false);
-                    return;
-                }
-
-                var canvas = document.createElement('canvas');
-                canvas.width = width;
-                canvas.height = height;
-
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0, width, height);
-
-                if (hasToBlobSupport) {
-                    canvas.toBlob(function (blob) {
-                        callback(blob, true);
-                    }, file.type);
-                } else {
-                    var blob = ImageTools._toBlob(canvas, file.type);
-                    callback(blob, true);
-                }
-            };
-            ImageTools._loadImage(image, file);
-
-            return true;
-        }
-    }, {
-        key: '_toBlob',
-        value: function _toBlob(canvas, type) {
-            var dataURI = canvas.toDataURL(type);
-            var dataURIParts = dataURI.split(',');
-            var byteString = undefined;
-            if (dataURIParts[0].indexOf('base64') >= 0) {
-                // Convert base64 to raw binary data held in a string:
-                byteString = atob(dataURIParts[1]);
-            } else {
-                // Convert base64/URLEncoded data component to raw binary data:
-                byteString = decodeURIComponent(dataURIParts[1]);
-            }
-            var arrayBuffer = new ArrayBuffer(byteString.length);
-            var intArray = new Uint8Array(arrayBuffer);
-
-            for (var i = 0; i < byteString.length; i += 1) {
-                intArray[i] = byteString.charCodeAt(i);
-            }
-
-            var mimeString = dataURIParts[0].split(':')[1].split(';')[0];
-            var blob = null;
-
-            if (hasBlobConstructor) {
-                blob = new Blob([hasArrayBufferViewSupport ? intArray : arrayBuffer], { type: mimeString });
-            } else {
-                var bb = new BlobBuilder();
-                bb.append(arrayBuffer);
-                blob = bb.getBlob(mimeString);
-            }
-
-            return blob;
-        }
-    }, {
-        key: '_loadImage',
-        value: function _loadImage(image, file, callback) {
-            if (typeof URL === 'undefined') {
-                var reader = new FileReader();
-                reader.onload = function (evt) {
-                    image.src = evt.target.result;
-                    if (callback) {
-                        callback();
-                    }
-                };
-                reader.readAsDataURL(file);
-            } else {
-                image.src = URL.createObjectURL(file);
-                if (callback) {
-                    callback();
-                }
-            }
-        }
-    }, {
-        key: 'isSupported',
-        value: function isSupported() {
-            return typeof HTMLCanvasElement !== 'undefined' && hasBlobSupport && hasReaderSupport;
-        }
-    }]);
-
-    return ImageTools;
-})();
-
-exports['default'] = ImageTools;
-module.exports = exports['default']; 
-/*** End ImageTools.js */
 
 /* Utility function to convert a canvas to a BLOB */
 var dataURLToBlob = function(dataURL) {
@@ -459,21 +276,6 @@ function blobToFile(theBlob, fileName){
           // Callback after fileReader loads the data with Url.
           this.image_url = fileReader.result
 
-          /*** Start using ImageTools to resize image***/
-          /*
-          ImageTools.resize(this.files[0], {
-            width: 320, // maximum width
-            height: 240 // maximum height
-          }, function(blob, didItResize) {
-          // didItResize will be true if it managed to resize it, otherwise false (and will return the original file as 'blob')
-          //document.getElementById('preview').src = window.URL.createObjectURL(blob);
-          this.image_url = window.URL.createObjectURL(blob);
-          console.log('did it resize: ', didItResize)
-          // you can also now upload this blob using an XHR.
-          });
-          */
-          /*** End using ImageTools***/
-
           /*** Start using Canvas to resize image ***/
           // holds the uploaded image, used to get dimensions
           var img = new Image()
@@ -491,7 +293,7 @@ function blobToFile(theBlob, fileName){
 
             // this is to resize the uploaded image
             // max width allowed = 1200, height allowed = 630
-            var resizeDims = self.resizeImage(img.width, img.height, 400, 210)
+            var resizeDims = self.resizeImage(img.width, img.height, 1200, 630) //400, 210
             img.width = resizeDims[0]
             img.height = resizeDims[1]
 
@@ -506,14 +308,17 @@ function blobToFile(theBlob, fileName){
             // dataURL = canvas.toDataURL('image/jpeg')
             //console.log('dataURL: ', dataURL)
 
+            // convert canvas to url
             self.resizedURL = canvas.toDataURL('image/jpeg')
             //console.log('resizedURL: ', self.resizedURL)
 
-            // a blob, a file is a type of blob
+            // convert url to a blob, a file is a type of blob
             var resizedImage = dataURLToBlob(self.resizedURL)
             console.log('resized image blob: ', resizedImage)
 
+            // convert blob to file by adding name and date
             var resizedFile = blobToFile(resizedImage, filename)
+            
             console.log('resized file name: ', resizedFile.name)
             console.log('resized image file: ', resizedImage)
 
@@ -575,36 +380,7 @@ function blobToFile(theBlob, fileName){
         this.resized_height = height
 
         return [width, height]
-      },
-
-      /* Utility function to convert a canvas to a BLOB */
-      /*
-      dataURLToBlob(dataURL) {
-          var BASE64_MARKER = ';base64,';
-          console.log('url in util:', dataURL)
-          if (dataURL.indexOf(BASE64_MARKER) == -1) {
-              var parts = dataURL.split(',');
-              var contentType = parts[0].split(':')[1];
-              var raw = parts[1];
-
-              return new Blob([raw], {type: contentType});
-          }
-
-          var parts = dataURL.split(BASE64_MARKER);
-          var contentType = parts[0].split(':')[1];
-          var raw = window.atob(parts[1]);
-          var rawLength = raw.length;
-
-          var uInt8Array = new Uint8Array(rawLength);
-
-          for (var i = 0; i < rawLength; ++i) {
-              uInt8Array[i] = raw.charCodeAt(i);
-          }
-
-          return new Blob([uInt8Array], {type: contentType});
       }
-      */
-      /* End Utility function to convert a canvas to a BLOB      */
 
     }
   }
