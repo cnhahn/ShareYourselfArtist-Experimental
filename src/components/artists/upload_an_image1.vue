57c582bcb2c9296732c14c1278416f36cc839a18
Unknown <mdhsieh@ucsc.edu>
2019-03-06 23:13:06 -0800

    using ImageTools to resize, resize 1
    failed

--- src/components/artists/upload_an_image1.vue
+++ src/components/artists/upload_an_image1.vue
@@ -145,6 +145,190 @@
 </template>
 
 <script>
+/*** Start ImageTools.js, https://gist.github.com/dcollien/312bce1270a5f511bf4a#file-imagetools-es6 */
+'use strict';
+
+if (typeof exports === "undefined") {
+    var exports = {};
+}
+
+if (typeof module === "undefined") {
+   var module = {};
+}
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var hasBlobConstructor = typeof Blob !== 'undefined' && (function () {
+    try {
+        return Boolean(new Blob());
+    } catch (e) {
+        return false;
+    }
+})();
+
+var hasArrayBufferViewSupport = hasBlobConstructor && typeof Uint8Array !== 'undefined' && (function () {
+    try {
+        return new Blob([new Uint8Array(100)]).size === 100;
+    } catch (e) {
+        return false;
+    }
+})();
+
+var hasToBlobSupport = typeof HTMLCanvasElement !== "undefined" ? HTMLCanvasElement.prototype.toBlob : false;
+
+var hasBlobSupport = hasToBlobSupport || typeof Uint8Array !== 'undefined' && typeof ArrayBuffer !== 'undefined' && typeof atob !== 'undefined';
+
+var hasReaderSupport = typeof FileReader !== 'undefined' || typeof URL !== 'undefined';
+
+var ImageTools = (function () {
+    function ImageTools() {
+        _classCallCheck(this, ImageTools);
+    }
+
+    _createClass(ImageTools, null, [{
+        key: 'resize',
+        value: function resize(file, maxDimensions, callback) {
+            if (typeof maxDimensions === 'function') {
+                callback = maxDimensions;
+                maxDimensions = {
+                    width: 640,
+                    height: 480
+                };
+            }
+
+            var maxWidth = maxDimensions.width;
+            var maxHeight = maxDimensions.height;
+
+            if (!ImageTools.isSupported() || !file.type.match(/image.*/)) {
+                callback(file, false);
+                return false;
+            }
+
+            if (file.type.match(/image\/gif/)) {
+                // Not attempting, could be an animated gif
+                callback(file, false);
+                // TODO: use https://github.com/antimatter15/whammy to convert gif to webm
+                return false;
+            }
+
+            var image = document.createElement('img');
+
+            image.onload = function (imgEvt) {
+                var width = image.width;
+                var height = image.height;
+                var isTooLarge = false;
+
+                if (width > height && width > maxDimensions.width) {
+                    // width is the largest dimension, and it's too big.
+                    height *= maxDimensions.width / width;
+                    width = maxDimensions.width;
+                    isTooLarge = true;
+                } else if (height > maxDimensions.height) {
+                    // either width wasn't over-size or height is the largest dimension
+                    // and the height is over-size
+                    width *= maxDimensions.height / height;
+                    height = maxDimensions.height;
+                    isTooLarge = true;
+                }
+
+                if (!isTooLarge) {
+                    // early exit; no need to resize
+                    callback(file, false);
+                    return;
+                }
+
+                var canvas = document.createElement('canvas');
+                canvas.width = width;
+                canvas.height = height;
+
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(image, 0, 0, width, height);
+
+                if (hasToBlobSupport) {
+                    canvas.toBlob(function (blob) {
+                        callback(blob, true);
+                    }, file.type);
+                } else {
+                    var blob = ImageTools._toBlob(canvas, file.type);
+                    callback(blob, true);
+                }
+            };
+            ImageTools._loadImage(image, file);
+
+            return true;
+        }
+    }, {
+        key: '_toBlob',
+        value: function _toBlob(canvas, type) {
+            var dataURI = canvas.toDataURL(type);
+            var dataURIParts = dataURI.split(',');
+            var byteString = undefined;
+            if (dataURIParts[0].indexOf('base64') >= 0) {
+                // Convert base64 to raw binary data held in a string:
+                byteString = atob(dataURIParts[1]);
+            } else {
+                // Convert base64/URLEncoded data component to raw binary data:
+                byteString = decodeURIComponent(dataURIParts[1]);
+            }
+            var arrayBuffer = new ArrayBuffer(byteString.length);
+            var intArray = new Uint8Array(arrayBuffer);
+
+            for (var i = 0; i < byteString.length; i += 1) {
+                intArray[i] = byteString.charCodeAt(i);
+            }
+
+            var mimeString = dataURIParts[0].split(':')[1].split(';')[0];
+            var blob = null;
+
+            if (hasBlobConstructor) {
+                blob = new Blob([hasArrayBufferViewSupport ? intArray : arrayBuffer], { type: mimeString });
+            } else {
+                var bb = new BlobBuilder();
+                bb.append(arrayBuffer);
+                blob = bb.getBlob(mimeString);
+            }
+
+            return blob;
+        }
+    }, {
+        key: '_loadImage',
+        value: function _loadImage(image, file, callback) {
+            if (typeof URL === 'undefined') {
+                var reader = new FileReader();
+                reader.onload = function (evt) {
+                    image.src = evt.target.result;
+                    if (callback) {
+                        callback();
+                    }
+                };
+                reader.readAsDataURL(file);
+            } else {
+                image.src = URL.createObjectURL(file);
+                if (callback) {
+                    callback();
+                }
+            }
+        }
+    }, {
+        key: 'isSupported',
+        value: function isSupported() {
+            return typeof HTMLCanvasElement !== 'undefined' && hasBlobSupport && hasReaderSupport;
+        }
+    }]);
+
+    return ImageTools;
+})();
+
+exports['default'] = ImageTools;
+module.exports = exports['default'];    
+/*** End ImageTools.js */
+
   // Styled by Jin. No modification on code.
   export default {
     name: 'myTour',
@@ -172,6 +356,7 @@
         image_is_landscape: true,
         resized_width: 0,
         resized_height: 0,
+        resizedURL: '',
         steps: [
           {
             target: '#v-step-0', 
@@ -240,16 +425,31 @@
           // Callback after fileReader loads the data with Url.
           this.image_url = fileReader.result
 
+          /*** Start using ImageTools to resize image***/
+          ImageTools.resize(this.files[0], {
+            width: 320, // maximum width
+            height: 240 // maximum height
+          }, function(blob, didItResize) {
+          // didItResize will be true if it managed to resize it, otherwise false (and will return the original file as 'blob')
+          //document.getElementById('preview').src = window.URL.createObjectURL(blob);
+          this.image_url = window.URL.createObjectURL(blob);
+          console.log('did it resize: ', didItResize)
+          // you can also now upload this blob using an XHR.
+          });
+          /*** End using ImageTools***/
+
+          /*** Start using Canvas to resize image ***/
+          /*
           // holds the uploaded image, used to get dimensions
           var img = new Image()
 
-          var dataURL = '' //this.image_url
+          // var dataURL = ''
           // image loading is done asynchronously, so you have to wait for load event
           // in other words, you have to wait for the image to load before using image
           img.onload = function()
           {
-            console.log('image width: ', img.width)
-            console.log('image height: ', img.height)
+            //console.log('image width: ', img.width)
+            //console.log('image height: ', img.height)
             
             // check orientation
             self.checkOrientation(img.width, img.height)
@@ -259,35 +459,29 @@
             var resizeDims = self.resizeImage(img.width, img.height, 1200, 630)
             img.width = resizeDims[0]
             img.height = resizeDims[1]
-            //console.log('resized image width: ', img.width)
-            //console.log('resized image height: ', img.height)
 
             // https://stackoverflow.com/questions/23945494/use-html5-to-resize-an-image-before-upload
             var canvas = document.createElement('canvas')
-            console.log('resized image width: ', img.width)
-            console.log('resized image height: ', img.height)
+            //console.log('resized image width: ', img.width)
+            //console.log('resized image height: ', img.height)
             canvas.width = img.width
             canvas.height = img.height
             canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height)
 
-            var extension = self.checkUrl(img.src)
-            //console.log('image extension: ', extension)
-
-            dataURL = canvas.toDataURL('image/jpeg')
-            //dataURL = canvas.toDataURL('img/jpeg')
-            //dataURL = canvas.toDataURL('jpeg')
-            //dataURL = canvas.toDataURL(this)
+            // dataURL = canvas.toDataURL('image/jpeg')
+            self.resizedURL = canvas.toDataURL('image/jpeg')
             //console.log('dataURL: ', dataURL)
-            /*var resizedImage = dataURLToBlob(dataURL)
-            console.log('resizedImage: ', resizedImage)*/
 
-            //self.$store.dispatch('image_being_uploaded', {file: self.file, image_url: dataURL})
+            //self.$store.dispatch('image_being_uploaded', {file: self.file, image_url: self.resizedURL})
+            console.log('resizedURL: ', self.resizedURL)
           }
           img.src = this.image_url
           //console.log('image_url: ', this.image_url)
+          */
+          /*** End using Canvas to resize image ***/
 
-          /* this.$store.dispatch('image_being_uploaded', {file: this.file, image_url: this.image_url}) */
-          this.$store.dispatch('image_being_uploaded', {file: this.file, image_url: dataURL})
+          this.$store.dispatch('image_being_uploaded', {file: this.file, image_url: this.image_url})
+          // this.$store.dispatch('image_being_uploaded', {file: this.file, image_url: dataURL})
         })
         fileReader.readAsDataURL(files[0])
       },
@@ -335,33 +529,8 @@
         height = Math.round(height)
         this.resized_width = width
         this.resized_height = height
-        //console.log('resized image width: ', this.resized_width)
-        //console.log('resized image height: ', this.resized_height)
 
         return [width, height]
-      },
-      checkUrl(url){
-        /*var arr = [ "jpeg", "jpg", "gif", "png" ];
-        var ext = url.substring(url.lastIndexOf(".")+1)
-        if($.inArray(ext,arr)){
-          alert("valid url");
-          return true;
-        }*/
-
-        /*
-        // Remove everything to the last slash in URL
-        url = url.substr(1 + url.lastIndexOf("/"))
-
-        // Break URL at ? and take first part (file name, extension)
-        url = url.split('?')[0]
-
-        // Sometimes URL doesn't have ? but #, so we should aslo do the same for #
-        url = url.split('#')[0]
-
-        // Now we have only extension
-        return url;*/
-
-        return url
       }
 
     }
