b779a3a20041b5eb5a845efc22c28ba376f2b958
Unknown <mdhsieh@ucsc.edu>
2019-04-08 19:29:24 -0700

    added sort to all submissions button
    sort when all submissions button clicked, in filter_submissions

--- src/components/business/submissions.vue
+++ src/components/business/submissions.vue
@@ -151,7 +151,7 @@
           }
           else
           {
-            // null, place at end as oldest submissions
+            // place null submissions at end, assuming these are older submissions
             console.log('time submitted not available')
             return -1
 
@@ -162,7 +162,7 @@
             else if (b === null)  // b is null? last
               return 1;*/
 
-            //return (b===null)-(a===null)
+            //return (a===null)-(b===null)
           }
         },
         // sort submissions by most recent upload date
@@ -195,6 +195,7 @@
           console.log('here are submissions: ' + this.submissions)
           console.log('here are master submissions: ' + this.master_submissions)
           this.submissions = this.$store.getters.submissions_for_this_business
+          this.sortByDate(this.submissions)
           this.master_submissions = this.$store.getters.submissions_for_this_business
 
           console.log('now here is submissions: ' + this.submissions)
